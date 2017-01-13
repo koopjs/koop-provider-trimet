719f1c259594bcdb2e909375ced2888c14641842
dmfenton <dfenton@esri.com>
2017-01-12 20:28:16 -0500

    fix route declaration

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
-module.exports = {
+const routes = {
   'get /trimet': 'index',
   'get /trimet/FeatureServer': 'featureServer',
   'get /trimet/FeatureServer/:layer': 'featureServer',
-  'pst /trimet/FeatureServer/:layer': 'featureServer',
+  'post /trimet/FeatureServer/:layer': 'featureServer',
   'get /trimet/FeatureServer/:layer/:method': 'featureServer',
   'post /trimet/FeatureServer/:layer/:method': 'featureServer'
 }
+
+module.exports = routes
