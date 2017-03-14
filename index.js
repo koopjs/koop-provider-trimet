dd7f5fa5308864a4676a3e7ddd619df2fe875465
dmfenton <dfenton@esri.com>
2017-03-13 22:22:18 -0400

    update for koop 3

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-// the name of provider is used by koop to help build default routes for FeatureService and a preview
 const pkg = require('./package.json')
 const provider = {
-  plugin_name: 'trimet',
+  name: 'trimet',
   hosts: false,
-  controller: require('./controller'),
-  model: require('./model'),
-  routes: require('./routes'),
-  status: {
-    version: pkg.version
-  },
+  Model: require('./model'),
+  version: pkg.version,
   type: 'provider'
 }
 
--- model.js
+++ model.js
@@ -1,30 +1,68 @@
-'use strict'
+/*
+  model.js
 
+  This file is required. It must export a class with at least one public function called `getData`
+
+  Documentation: http://koopjs.github.io/docs/specs/provider/
+*/
 const request = require('request').defaults({gzip: true, json: true})
+const config = require('config')
 const translate = require('./translate')
-// Set up a POJO cache that gets cleared every 5 seconds
-const cache = {}
-const interval = setInterval(() => { delete cache.data }, 5000)
 
-process.on('SIGINT', () => {
-  clearInterval(interval)
-  process.exit(0)
-})
-process.on('SIGTERM', () => {
-  clearInterval(interval)
-  process.exit(0)
-})
+function Model (koop) {}
 
-module.exports = function (koop) {
-  return {
-    get (callback) {
-      if (cache.data) return callback(null, cache.data)
-      request('https://developer.trimet.org/ws/v2/vehicles/onRouteOnly/false/appid/8A0EBB788E8205888807BAC97', (err, res, body) => {
-        if (err) return callback(err)
-        const featureCollection = translate(body)
-        callback(null, featureCollection)
-        cache.data = featureCollection
-      })
-    }
-  }
+// This is the only public function you need to implement
+Model.prototype.getData = function (req, callback) {
+  // Call the remote API with our developer key
+  const key = config.trimet.key
+  request(`https://developer.trimet.org/ws/v2/vehicles/onRouteOnly/false/appid/${key}`, (err, res, body) => {
+    if (err) return callback(err)
+    // translate the response into geojson
+    const geojson = translate(body)
+    // Cache data for 10 seconds at a time by setting the ttl or "Time to Live"
+    geojson.ttl = 10
+    // hand off the data to Koop
+    callback(null, geojson)
+  })
 }
+
+module.exports = Model
+
+/* Example raw API response
+{
+  "resultSet": {
+  "queryTime": 1488465776220,
+  "vehicle": [
+    {
+      "expires": 1488466246000,
+      "signMessage": "Red Line to Beaverton",
+      "serviceDate": 1488441600000,
+      "loadPercentage": null,
+      "latitude": 45.5873117,
+      "nextStopSeq": 1,
+      "source": "tab",
+      "type": "rail",
+      "blockID": 9045,
+      "signMessageLong": "MAX  Red Line to City Center & Beaverton",
+      "lastLocID": 10579,
+      "nextLocID": 10579,
+      "locationInScheduleDay": 24150,
+      "newTrip": false,
+      "longitude": -122.5927705,
+      "direction": 1,
+      "inCongestion": null,
+      "routeNumber": 90,
+      "bearing": 145,
+      "garage": "ELMO",
+      "tripID": "7144393",
+      "delay": -16,
+      "extraBlockID": null,
+      "messageCode": 929,
+      "lastStopSeq": 26,
+      "vehicleID": 102,
+      "time": 1488465767051,
+      "offRoute": false
+    }
+  ]
+}
+*/
