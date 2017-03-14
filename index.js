93296f519204743121536a8c688c305e321de466
dmfenton <dfenton@esri.com>
2017-03-14 14:52:21 -0400

    add config

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const pkg = require('./package.json')
 const provider = {
   name: 'trimet',
   hosts: false,
+  disableIdParam: true,
   Model: require('./model'),
   version: pkg.version,
   type: 'provider'
